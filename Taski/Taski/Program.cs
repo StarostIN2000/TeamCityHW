4dcb86f7526e37a0b417cd96666ccee2b18f7d88
StarostIN2000 <72708700+StarostIN2000@users.noreply.github.com>
2023-12-13 17:58:38 +0300

    Add script

---
+++ Taski/Taski/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public double Aboba(int n)
+    {
+        return Math.Pow(2, n);
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Привет, мир!");
+
+        Console.WriteLine($"2^10 = " + Math.Pow(2, 10));
+
+        Console.WriteLine("Нажми любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+
+    
+}
